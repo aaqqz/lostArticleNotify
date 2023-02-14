26bcdbc959c37e52082e31b7e4a165d15dd1edac
HK-Kim <sswwx1@naver.com>
2023-02-15 01:17:00 +0900

    jwt exception

--- src/main/java/project/toy/api/config/AuthResolver.java
+++ src/main/java/project/toy/api/config/AuthResolver.java
@@ -1,8 +1,6 @@
 package project.toy.api.config;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
@@ -11,6 +9,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import project.toy.api.config.data.MemberSession;
+import project.toy.api.exception.JwtAuthFail;
 import project.toy.api.exception.Unauthorized;
 
 @Slf4j
@@ -35,17 +34,33 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
             throw new Unauthorized();
         }
 
-        Jws<Claims> claims = Jwts.parserBuilder()
-                .setSigningKey(jwtConfig.getJwtKey())
-                .build()
-                .parseClaimsJws(jws);
+        Jws<Claims> claims = null;
+        try {
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(jwtConfig.getJwtKey())
+                    .build()
+                    .parseClaimsJws(jws);
+
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.error("잘못된 JWT 서명입니다.");
+            throw new JwtAuthFail("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.error("만료된 JWT 토큰입니다.");
+            throw new JwtAuthFail("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.error("지원되지 않는 JWT 토큰입니다.");
+            throw new JwtAuthFail("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.error("JWT 토큰이 잘못되었습니다.");
+            throw new JwtAuthFail("JWT 토큰이 잘못되었습니다.");
+        }
 
         log.info("claims >>> {}", claims);
         Long memberId = Long.parseLong(claims.getBody().getSubject());
         String memberName = String.valueOf(claims.getBody().get("name"));
+        int iat = (int) claims.getBody().get("iat");
+        int exp = (int) claims.getBody().get("exp");
 
-        log.info("claims.memberId >>> {}", memberId);
-        log.info("claims.name >>> {}", memberName);
         return new MemberSession(memberId, memberName);
     }
 }
