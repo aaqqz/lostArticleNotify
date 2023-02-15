e599e9c6aa20533832ca24b08915f9f206d8f3a7
kh-kim <sswwx1@naver.com>
2023-02-15 17:47:42 +0900

    Scheduler 작업중 커밋, return 분기 필요

--- src/main/java/project/toy/ToyApplication.java
+++ src/main/java/project/toy/ToyApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import project.toy.api.config.JwtConfig;
 
 @EnableConfigurationProperties(JwtConfig.class)
 @EnableJpaAuditing // BaseEntity 설정
+@EnableScheduling
 @SpringBootApplication
 public class ToyApplication {
 
